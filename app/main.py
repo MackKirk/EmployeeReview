4987eaefc7b2cbcc0b439843cb85c7edfef5b98b
RaphVCC <124946896+RaphVCC@users.noreply.github.com>
2025-06-27 16:04:10 -0700

    main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
-from app.routes import auth
+from app.routes import auth, employee, supervisor, director  # certifique-se de importar tudo
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="changeme")
 
 app.include_router(auth.router)
+app.include_router(auth.router)
+app.include_router(employee.router)
+app.include_router(supervisor.router)
+app.include_router(director.router)
