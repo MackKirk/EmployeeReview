a5212a77d3153883de3875751e5ab7f2517fa6ad
RaphVCC <124946896+RaphVCC@users.noreply.github.com>
2025-07-02 11:24:58 -0700

    Static Files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
 from starlette.middleware.sessions import SessionMiddleware
 from app.routes import auth, employee, supervisor, director, home
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="changeme")
+
+# Garante que a pasta está montada corretamente
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth.router)
 app.include_router(employee.router)
