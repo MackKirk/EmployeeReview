a518c384a1c6ec58556d9181516e31ba56453d74
RaphVCC <raph.vcc@gmail.com>
2025-09-02 13:38:35 -0700

    Add database table creation on startup event

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import os
 from starlette.middleware.sessions import SessionMiddleware
 import os
 from app.routes import auth, employee, supervisor, director, home
+from app.db import engine
+from app.models import Base
 
 app = FastAPI()
 SESSION_SECRET = os.getenv("SESSION_SECRET", "changeme")
@@ -17,3 +19,8 @@ app.include_router(employee.router)
 app.include_router(supervisor.router)
 app.include_router(director.router)
 app.include_router(home.router)
+
+
+@app.on_event("startup")
+def create_tables_on_startup():
+    Base.metadata.create_all(bind=engine)
